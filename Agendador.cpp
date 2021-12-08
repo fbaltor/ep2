f8c97cd4887c4b4a20dea9583e14010a04465f98
Felipe Baltor <fbaltor@gmail.com>
2021-12-07 23:49:59 -0300

    wip

--- Agendador.cpp
+++ Agendador.cpp
@@ -5,3 +5,11 @@
 
 Agendador::Agendador(int instanteInicial, Rede *rede, int tamanho)
     : instanteInicial(instanteInicial), rede(rede), tamanho(tamanho) {}
+
+Agendador::~Agendador() {}
+
+bool Agendador::agendar(int instante, No *n, Datagrama *d) {}
+
+void Agendador::processar() {}
+
+int Agendador::getInstante() { return instanteInicial; }
